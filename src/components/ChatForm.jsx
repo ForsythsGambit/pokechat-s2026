32a2225f956e1746863dffc7551dd4a8f8cd4d31
Liam Crowley <theadvfred@gmail.com>
2026-04-26 22:17:00 -0500

    WIP on chat form

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,21 +1,29 @@
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Card, Icon, Image, Input, List, Label} from 'semantic-ui-react'
 import axios from 'axios';
 import {CHAT_API} from '../AppConfig';
+import { compileString } from 'sass';
 
 // HANDLES INTERACTIONS WITH THE LLM (/backend)
 const ChatForm = ({setSearchResults})=>{
-
-    const chat = (query)=>{
-        // AXIOS GET on the POKECHAT API POINT 
+	const [query, setQuery] = useState('');
+    const chat = ()=>{
+		if (!query)
+			return;
+		axios.get(`${CHAT_API}/chat/hello`, {params: {query}})
+		.then( (response) => {setQuery(response.data); console.log(response.data); } )
+		.catch((error) => (console.error(error)));
     }
+
 
     return (
     <div className='chat'>
         <Input fluid 
-        icon={<Icon name='send' inverted circular link />}
-        placeholder='Ask me a Pokemon Question...'
+			icon={<Icon name='send' inverted circular link />}
+			placeholder='Ask me a Pokemon Question...'
+			onChange={(msg) => setQuery(msg.target.value)}
+			onKeyPress={(e) => e.key === 'Enter' && chat()}
         />
         <Label pointing='above' message="strongest pokemon limit 1"> Strongest Pokemon </Label>
         <Label pointing='above' message="weakest pokemon limit 1"> Weakest Pokemon </Label>
