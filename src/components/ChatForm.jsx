09d122147dd33d822156ef461a4a856452864dc9
Liam Crowley <theadvfred@gmail.com>
2026-04-26 22:38:58 -0500

    Fixed input & label

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -11,23 +11,25 @@ const ChatForm = ({setSearchResults})=>{
     const chat = ()=>{
 		if (!query)
 			return;
-		axios.get(`${CHAT_API}/chat/hello`, {params: {query}})
-		.then( (response) => {setQuery(response.data); console.log(response.data); } )
-		.catch((error) => (console.error(error)));
+		console.log(query)
+		axios.get(`${CHAT_API}/chat/query`, {params: {query}})
+		.then( (response) => {setQuery(response); console.log(response); } )
+		.catch((error) => (console.log(error)));
     }
 
 
     return (
     <div className='chat'>
         <Input fluid 
-			icon={<Icon name='send' inverted circular link />}
+			icon={<Icon name='send' inverted circular link onClick={chat} />}
 			placeholder='Ask me a Pokemon Question...'
+			value = { query ? query  : ""}
 			onChange={(msg) => setQuery(msg.target.value)}
 			onKeyPress={(e) => e.key === 'Enter' && chat()}
         />
-        <Label pointing='above' message="strongest pokemon limit 1"> Strongest Pokemon </Label>
-        <Label pointing='above' message="weakest pokemon limit 1"> Weakest Pokemon </Label>
-        <Label pointing='above' message="starter pokemon limit 3"> Starter Pokemon </Label>
+        <Label pointing='above' message="strongest pokemon limit 1" onClick={() => {setQuery("Strongest pokemon limit 1"); chat();}}> Strongest Pokemon </Label>
+        <Label pointing='above' message="weakest pokemon limit 1" onClick={() => {setQuery("weakest pokemon limit 1"); chat();}}> Weakest Pokemon </Label>
+        <Label pointing='above' message="starter pokemon limit 3" onClick={() => {setQuery("starter pokemon limit 3"); chat();}}> Starter Pokemon </Label>
     </div>
     );
 }
